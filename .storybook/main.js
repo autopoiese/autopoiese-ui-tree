ebeb11b87c2050fe8d2cdb2f2dcf16ff83e73d93
Jonathan Allenberg <git@jfa.sh>
2022-06-13 14:19:29 +0200

    ...

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,10 @@
-const rootMain = require('../../../../.storybook/main');
+const rootMain = require('../../../../.storybook/main')
 
 // Use the following syntax to add addons!
 // rootMain.addons.push('');
-rootMain.stories.push(...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)'])
+rootMain.stories.push(
+  ...['../src/lib/**/*.stories.mdx', '../src/lib/**/*.stories.@(js|jsx|ts|tsx)']
+)
+rootMain.core = { ...rootMain.core, builder: 'webpack5' }
 
-module.exports = rootMain;
+module.exports = rootMain
